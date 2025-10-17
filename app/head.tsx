49f28b76ddeb45728f27559ef316512f75b8960d
Codeus1 <tonytorres1098@gmail.com>
2025-10-17 23:56:15 +0200

    feat: update Google site verification meta tag for improved SEO

--- app/head.tsx
+++ app/head.tsx
@@ -3,8 +3,9 @@ export default function Head() {
     <>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="google-site-verification" content="jmPFmozsvJAi_hjpT29gB7RBbnLPzIMTATGGfiaQUV8" />
+      {/* <meta name="google-site-verification" content="jmPFmozsvJAi_hjpT29gB7RBbnLPzIMTATGGfiaQUV8" /> */}
       <link rel="icon" href="/favicon.ico" />
+      <meta name="google-site-verification" content="TsVpjtEQWVGyYZFRYTPXF7IeaaBv28OrI5vhBv6logM" />
     </>
   )
 }
